e8e2da72f226fd4445b2f475d8abcd91d639e876
joaop21 <jpsilva9898@gmail.com>
2022-06-20 23:37:22 +0100

    ZSH - Add Z

--- zsh/.zprofile
+++ zsh/.zprofile
@@ -7,3 +7,6 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 # Openssl3 - flags for compilers
 LDFLAGS="-L/opt/homebrew/opt/openssl@3/lib"
 CPPFLAGS="-I/opt/homebrew/opt/openssl@3/include"
+
+# Z
+. /opt/homebrew/etc/profile.d/z.sh
--- zsh/plugins/.plugins.zsh
+++ zsh/plugins/.plugins.zsh
@@ -3,9 +3,6 @@ export ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 
 source $DOTFILES/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 
-# z
-source $DOTFILES/zsh/plugins/z/z.sh
-
 # ... other plugins ...
 
 # syntax highlighting - SHOULD BE AT THE END OF THIS FILE
