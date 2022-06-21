45fd15523eb51924bab62d4f53b92886c7a4ec67
joaop21 <jpsilva9898@gmail.com>
2022-06-21 23:58:05 +0100

    Aliases - Add more git alias and change ' to "

--- system/aliases.sh
+++ system/aliases.sh
@@ -2,32 +2,34 @@
 # | Git |
 # +-----+
 
-alias g='git'
-alias ga='git add'
-alias gc='git commit'
-alias gcam='git commit -am'
-alias gch='git checkout'
-alias gchb='git checkout -b'
-alias gcl='git clone'
-alias gd='git diff'
-alias gl='git log'
-alias glog='g log --oneline --graph --decorate'
-alias gp='git push'
-alias gp='git push -f'
-alias gpl='git pull'
-alias gs='git status'
-alias gst='git status'
+alias g="git"
+alias ga="git add"
+alias gc="git commit"
+alias gcam="git commit -am"
+alias gcm="git commit -m"
+alias gch="git checkout"
+alias gchb="git checkout -b"
+alias gchm="git checkout main"
+alias gcl="git clone"
+alias gd="git diff"
+alias gl="git log"
+alias glog="g log --oneline --graph --decorate"
+alias gp="git push"
+alias gpup="git push --set-upstream origin $(git branch --show-current)"
+alias gpl="git pull"
+alias gs="git status"
+alias gst="git status"
 
 # +----+
 # | ls |
 # +----+
 
-alias ll='exa -alh --git --icons'
+alias ll="exa -alh --git --icons"
 
 # +------+
 # | nvim |
 # +------+
 
-alias nv='nvim'
-alias vi='nvim'
-alias vim='nvim'
+alias nv="nvim"
+alias vi="nvim"
+alias vim="nvim"
