b16317c0b92e207b965a5aacd0a43c9c790d574a
joaop21 <jpsilva9898@gmail.com>
2023-10-31 12:13:38 +0000

    add terminal plugin

---
+++ nvim/lua/plugins/terminal.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      open_mapping = [[<c-t>]],
+      direction = "float",
+      float_opts = {
+        border = "curved",
+      },
+      autochdir = true,
+    }
+  }
+}
