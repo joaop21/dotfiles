f575560feb1961e54a78c0a4220fcaf5a89f3aff
João <jpsilva9898@gmail.com>
2024-04-09 00:13:53 +0100

    Nvim - Add Bufferline and mini.bufremove (#15)

--- nvim/lua/plugins/file-explorer.lua
+++ nvim/lua/plugins/file-explorer.lua
@@ -1,40 +1,42 @@
 return {
-  -- file explorer
-  {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    lazy = true,
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    opts = {
-      hijack_cursor = true,
-      disable_netrw = true,
-      sync_root_with_cwd = true,
-      view = {
-        side = "right",
-        preserve_window_proportions = true,
-      },
-      filters = {
-        custom = { "^\\.DS_Store" }
-      },
-    },
-    keys = {
-      {
-        '<leader>nn',
-        function() require('nvim-tree.api').tree.toggle() end,
-        desc = "Toggle"
-      },
-      {
-        '<leader>nf',
-        function()
-            require('nvim-tree.api').tree.find_file({
-                open = true,
-                focus = true
-            })
-        end,
-        desc = "Find file"
-      }
-    }
-  }
+	-- file explorer
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = true,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		opts = {
+			hijack_cursor = true,
+			disable_netrw = true,
+			sync_root_with_cwd = true,
+			view = {
+				side = "right",
+				preserve_window_proportions = true,
+			},
+			filters = {
+				custom = { "^\\.DS_Store" },
+			},
+		},
+		keys = {
+			{
+				"<leader>nn",
+				function()
+					require("nvim-tree.api").tree.toggle()
+				end,
+				desc = "Toggle",
+			},
+			{
+				"<leader>nf",
+				function()
+					require("nvim-tree.api").tree.find_file({
+						open = true,
+						focus = true,
+					})
+				end,
+				desc = "Find file",
+			},
+		},
+	},
 }
