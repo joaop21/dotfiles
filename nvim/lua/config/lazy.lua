fed0c6125bb0178347c9d7089d5ca658a54e5f86
joaop21 <jpsilva9898@gmail.com>
2025-04-11 14:58:40 +0100

    add biome and config to not conflict with prettier

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -15,6 +15,7 @@ require("lazy").setup({
     -- import any extras modules
     { import = "lazyvim.plugins.extras.coding.luasnip" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
+    { import = "lazyvim.plugins.extras.formatting.biome" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.elixir" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
