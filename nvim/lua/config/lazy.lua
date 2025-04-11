257c1fc147dc14711b1b2d8c471f50f3983f3586
João <jpsilva9898@gmail.com>
2025-04-11 15:10:15 +0100

    Nvim - Add biome extra (#34)
    It also:
    - removes eslint;
    - updates some packages;

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -15,12 +15,12 @@ require("lazy").setup({
     -- import any extras modules
     { import = "lazyvim.plugins.extras.coding.luasnip" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
+    { import = "lazyvim.plugins.extras.formatting.biome" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.elixir" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
     { import = "lazyvim.plugins.extras.test.core" },
     -- import/override with your plugins
