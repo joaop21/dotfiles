56968cadc733c84cd4fb8354ecca3234f0cca0ae
joaop21 <jpsilva9898@gmail.com>
2026-03-13 23:14:44 +0000

    fix lsp config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,13 +3,8 @@ return {
   version = "*",
   opts = {
     servers = {
-      expert = {
-        cmd = { "/Users/joao/.local/share/nvim/mason/bin/expert", "--stdio" },
-        root_dir = function(fname)
-          return require("lspconfig.util").root_pattern("mix.exs", ".git")(fname)
-        end,
-        filetypes = { "elixir", "eelixir", "heex" },
-      },
+      elixirls = { enabled = false },
+      expert = {},
     },
   },
 }
