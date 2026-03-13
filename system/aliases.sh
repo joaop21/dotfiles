ca8a2365d443a0aec89a1a6d9d4bd65070c18474
joaop21 <jpsilva9898@gmail.com>
2026-03-13 18:00:35 +0000

    add some useful aliases

--- system/aliases.sh
+++ system/aliases.sh
@@ -10,6 +10,9 @@ alias cat='bat --paging=never'
 
 alias g='git'
 alias ga='git add'
+alias gamend='git commit --amend --no-edit'
+alias gb='git branch'
+alias gbd='git branch -d'
 alias gc='git commit'
 alias gcam='git commit -am'
 alias gcm='git commit -m'
@@ -18,13 +21,27 @@ alias gchb='git checkout -b'
 alias gchm='git checkout main'
 alias gcl='git clone'
 alias gd='git diff'
+alias gdf='git diff --staged'
 alias gl='git log'
+alias glast='git log -1 --stat'
 alias glog='g log --oneline --graph --decorate'
 alias gp='git push'
-alias gpup='git push --set-upstream origin $(git branch --show-current)'
 alias gpl='git pull'
+alias gpup='git push --set-upstream origin $(git branch --show-current)'
+alias grb='git rebase'
+alias grbm='git rebase main'
 alias gs='git status'
+alias gsp='git stash pop'
+alias gss='git stash'
 alias gst='git status'
+alias gsw='git switch'
+alias gundo='git reset HEAD~1 --soft'
+
+# +----+
+# | gh |
+# +----+
+
+alias gprw='gh pr create --web'
 
 # +----+
 # | ls |
