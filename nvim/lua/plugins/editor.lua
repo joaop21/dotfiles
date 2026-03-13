38f6520b66ea541344ff43b166862adebaf10431
João <jpsilva9898@gmail.com>
2026-03-13 19:01:12 +0000

    Nvim - Clean nvim comments and old config (#45)

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,43 +1,4 @@
 return {
-  -- file explorer
-  -- {
-  --   "nvim-neo-tree/neo-tree.nvim",
-  --   keys = {
-  --     { "<leader>e", false },
-  --     { "<leader>fe", false },
-  --     { "<leader>E", false },
-  --     { "<leader>fE", false },
-  --     {
-  --       "<leader>ee",
-  --       function()
-  --         require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root() })
-  --       end,
-  --       desc = "Explorer NeoTree (Root Dir)",
-  --     },
-  --     {
-  --       "<leader>ef",
-  --       function()
-  --         require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd(), reveal = true })
-  --       end,
-  --       desc = "Explore Current File NeoTree (cwd)",
-  --     },
-  --   },
-  --   opts = {
-  --     filesystem = {
-  --       filtered_items = {
-  --         hide_dotfiles = false,
-  --         always_show_by_pattern = { -- uses glob style patterns
-  --           ".env*",
-  --         },
-  --       },
-  --     },
-  --     window = {
-  --       position = "float",
-  --     },
-  --   },
-  -- },
-
-  -- Snacks
   {
     "folke/snacks.nvim",
     opts = {
@@ -62,40 +23,4 @@ return {
       },
     },
   },
-
-  -- Fuzzy finder
-  -- {
-  --   "nvim-telescope/telescope.nvim",
-  --   keys = {
-  --     { "<leader>,", false },
-  --     { "<leader>/", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
-  --     { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
-  --   },
-  --   opts = {
-  --     defaults = {
-  --       file_ignore_patterns = {
-  --         -- "out/",
-  --         -- "cache/",
-  --         -- "node_modules",
-  --         -- "%.git/",
-  --         "%_build/",
-  --         "%.elixir%_ls",
-  --         "deps",
-  --       },
-  --       layout_config = {
-  --         prompt_position = "top",
-  --       },
-  --       sorting_strategy = "ascending",
-  --     },
-  --   },
-  -- },
-  --
-  -- {
-  --   "ibhagwan/fzf-lua",
-  --   keys = {
-  --     { "<leader>,", false },
-  --     { "<leader>/", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
-  --     { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
-  --   },
-  -- },
 }
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,59 +1,9 @@
 return {
-  -- "neovim/nvim-lspconfig",
-  -- opts = {
-  --   inlay_hints = { enabled = false },
-  --   servers = {
-  --     eslint = {
-  --       root_dir = function()
-  --         local lazyvimRoot = require("lazyvim.util.root")
-  --         return lazyvimRoot.git()
-  --       end,
-  --     },
-  --     ts_ls = {
-  --       root_dir = function()
-  --         local lazyvimRoot = require("lazyvim.util.root")
-  --         return lazyvimRoot.git()
-  --       end,
-  --     },
-  --     vtsls = {
-  --       root_dir = function()
-  --         local lazyvimRoot = require("lazyvim.util.root")
-  --         return lazyvimRoot.git()
-  --       end,
-  --     },
-  --   },
-  -- },
-  -- "neovim/nvim-lspconfig",
-  -- version = "*",
-  -- opts = {
-  --   servers = {
-  --     eslint = {
-  --       settings = {
-  --         useFlatConfig = true, -- set if using flat config
-  --         experimental = {
-  --           useFlatConfig = nil, -- option not in the latest eslint-lsp
-  --         },
-  --       },
-  --     },
-  --   },
-  --   setup = {
-  --     eslint = function()
-  --       require("lazyvim.util").lsp.on_attach(function(client)
-  --         if client.name == "eslint" then
-  --           client.server_capabilities.documentFormattingProvider = true
-  --         elseif client.name == "tsserver" or client.name == "vtsls" then
-  --           client.server_capabilities.documentFormattingProvider = false
-  --         end
-  --       end)
-  --     end,
-  --   },
-  -- },
   "neovim/nvim-lspconfig",
   version = "*",
   opts = {
     servers = {
       expert = {
-        -- cmd = { "/Users/joao/Desktop/Projects/expert/apps/expert/burrito_out/expert_darwin_arm64", "--stdio" },
         cmd = { "/Users/joao/.local/share/nvim/mason/bin/expert", "--stdio" },
         root_dir = function(fname)
           return require("lspconfig.util").root_pattern("mix.exs", ".git")(fname)
